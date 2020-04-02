a7cd3205812aaf814174daf755bb9d2f0fb550fe
Matt Hackmann <matt@dxprog.com>
2020-04-01 23:56:33 -0700

    fixed css output for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,50 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isDev = process.env.NODE_ENV !== 'production';
+module.exports = (env, argv) => {
+  const isDev = argv.mode !== 'production';
 
-module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
+  return {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'index.css'
+      })
     ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'index.css'
-    })
-  ],
-};
+  };
+}
