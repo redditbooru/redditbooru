d785e9f7d339889ac6c66d8e59947bbce7c9ee3d
Matt Hackmann <matt@dxprog.com>
2020-04-01 23:47:21 -0700

    initial commit + most webpack work

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const el = document.getElementById('main');
+ReactDOM.render(<div>Hello, world</div>, el);
