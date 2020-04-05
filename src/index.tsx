65130f876d5bafff59102c46c0ce4c606a73f5c0
Matt Hackmann <matt@dxprog.com>
2020-04-04 22:22:51 -0700

    some react router hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import './index.scss';
 
-const el = document.getElementById('main');
-ReactDOM.render(<div>Hello, world</div>, el);
+const el = document.getElementById('app');
+ReactDOM.render((
+  <Router>
+    <h1>Redditbooru</h1>
+    <Link to="/upload">Do it</Link>
+    <Switch>
+      <Route path="/upload">
+        Upload
+      </Route>
+    </Switch>
+  </Router>
+), el);
