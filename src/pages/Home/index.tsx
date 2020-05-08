bdb4fbc5622dc2c50dc57f51fb5ba6de77bedfc9
Matt Hackmann <matt@dxprog.com>
2020-05-07 19:54:32 -0700

    fixed XHR loop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,9 +7,11 @@ import { IPostData } from '../../interfaces/api';
 export const Home = () => {
   const [ imageList, setImageList ] = useState<Array<IPostData> | undefined>();
 
-  fetch('//redditbooru.com/images/?sources=1', { mode: 'cors' })
-    .then(response => response.json())
-    .then(images => setImageList(images));
+  if (!imageList) {
+    fetch('//redditbooru.com/images/?sources=1', { mode: 'cors' })
+      .then(response => response.json())
+      .then(images => setImageList(images));
+  }
 
   return (
     <div id="home">
